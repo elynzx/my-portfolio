d23770c0b46d5b146e169237fb439574184afe80
Elynzx <rpascual06@outlook.com>
2026-03-06 21:08:52 -0500

    feat: add FileNavigation component with tab navigation

---
+++ src/components/FileNavigation/index.tsx
@@ -0,0 +1,32 @@
+import { TabButton } from './components/TabButton'
+
+const NAV_TABS = [
+  { label: 'Home', id: 'home' },
+  { label: 'Projects', id: 'projects' },
+  { label: 'Certifications', id: 'certifications' },
+  { label: 'Contact', id: 'contact' },
+]
+
+interface Props {
+  openFile: string
+  changeFileById: (tabId: string) => void
+}
+
+export const FileNavigation = ({ openFile, changeFileById }: Props) => {
+
+  return (
+
+    <nav className="relative" aria-label="Main navigation">
+      <div className="flex flex-col md:flex-row gap-y-2 mt-8 md:gap-x-4 md:gap-y-0 z-0">
+        {NAV_TABS.map((tab) => (
+          <TabButton
+            isActive={openFile === tab.id}
+            onClick={() => changeFileById(tab.id)}
+            label={tab.label}
+            key={tab.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
+}
