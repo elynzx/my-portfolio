46bc419e1f73c1825fa8a527747754c477a7579b
Elynzx <rpascual06@outlook.com>
2026-03-10 21:57:29 -0500

    feat: enhance Hero component with badges and tech stack icons

--- src/features/Home/sections/Hero/index.tsx
+++ src/features/Home/sections/Hero/index.tsx
@@ -1,6 +1,9 @@
 import ProfilePhoto from "@/assets/home/ProfilePhoto/Profile_01.png";
 import HiIcon from "@/assets/home/hi.png";
 import { Badge } from './components/Badge'
+import { FaMedal, FaCertificate, FaUsers, FaReact, FaDocker } from "react-icons/fa";
+import { SiTypescript, SiSpringboot, SiPython } from "react-icons/si";
+import { StackIcon } from './components/StackIcon'
 
 const about = {
   name: "Evelyn",
@@ -14,29 +17,64 @@ const about = {
   },
 };
 
+const badges = [
+  {
+    icon: <FaMedal />,
+    text: "Top 10% student"
+  },
+  {
+    icon: <FaCertificate />,
+    text: "English B2 certified"
+  },
+  {
+    icon: <FaUsers />,
+    text: "IEEE WIE volunteer"
+  },
+]
+
+const techStack = [
+  { icon: <FaReact /> },
+  { icon: <SiTypescript /> },
+  { icon: <SiSpringboot /> },
+  { icon: <SiPython /> },
+  { icon: <FaDocker /> },
+];
+
+
 export const Hero = () => {
   return (
     <section className="flex flex-col items-center justify-center p-6">
-      <div className="flex flex-col text-xl md:flex-row md:items-end ">
+      <div className="flex items-center text-xl gap-2 md:flex-row md:items-end ">
         <img
           src={about.images.hi}
           alt={"Greeting from " + about.name}
-          className="w-20 md:w-32 jello-vertical"
+          className="w-15 md:w-32 jello-vertical"
         />
-        <h2 className="text-xl font-semibold md:text-4xl md:mb-5 md:ml-2 text-darkPink">
-          I'm {about.name}
-        </h2>
+        <div className="flex flex-col">
+          <h2 className="text-lg font-semibold md:text-4xl md:mb-5 md:ml-2 text-darkPink">
+            I'm {about.name}
+          </h2>
+          <span className="text-sm font-semibold md:text-4xl ">{about.role}</span>
+        </div>
       </div>
-      <span className="text-lg font-semibold md:text-4xl ">{about.role}</span>
       <img
         src={about.images.profile}
         alt={"Profile photo of " + about.name}
-        className="h-auto w-40 md:w-90 z-10 md:mb-6"
+        className="h-auto w-40 rounded-full md:w-90 z-10 md:mb-6"
       />
-      <div className="flex text-justify items-center flex-col mt-4 md:mt-0 md:flex-row md:mb-16">
-        <p className="md:text-[17px] md:mr-28">{about.description}</p>
-        <div className="">
-          <Badge />
+
+      <div className="flex flex-row gap-4 mt-6 items-center justify-center">
+        {techStack.map((tech, idx) => (
+          <StackIcon key={idx} icon={tech.icon} />
+        ))}
+      </div>
+
+      <div className="flex text-justify items-center flex-col mt-6 gap-2 md:mt-0 md:flex-row md:mb-16">
+        <p className="italic border-l-4 pl-2 text-white md:text-[17px] md:mr-28">{about.description}</p>
+        <div className="flex flex-col gap-3 mt-4">
+          {badges.map((badge, idx) => (
+            <Badge key={idx} icon={badge.icon} text={badge.text} />
+          ))}
         </div>
       </div>
     </section>
