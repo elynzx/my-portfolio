238782532d4ea5a23dab23dab06d51b36208e36c
Elynzx <rpascual06@outlook.com>
2026-03-11 11:07:09 -0500

    fix: update Badge component styles for improved responsiveness

--- src/features/Home/sections/Hero/components/Badge.tsx
+++ src/features/Home/sections/Hero/components/Badge.tsx
@@ -8,7 +8,7 @@ interface Props {
 export const Badge = ({ text, icon
 }: Props) => {
   return (
-    <div className="flex items-center text-xs gap-2 justify-center bg-darkPink/20 text-center font-title px-3 py-1 rounded-full md:text-2xl md:mb-4 ">
+    <div className="flex items-center text-xs gap-2 justify-center bg-darkPink/20 text-center font-title px-3 py-1 rounded-full md:text-lg md:mb-4 md:px-6">
       <div className="text-lg">{icon}</div><p>{text}</p>
     </div>
   )
