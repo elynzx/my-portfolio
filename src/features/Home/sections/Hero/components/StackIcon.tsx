1ea9ea5ae4243e8368868f4194e3d63788de5fb4
Elynzx <rpascual06@outlook.com>
2026-03-11 11:22:40 -0500

    fix(StackIcon): increase hover scale effect for better interactivity

--- src/features/Home/sections/Hero/components/StackIcon.tsx
+++ src/features/Home/sections/Hero/components/StackIcon.tsx
@@ -7,7 +7,7 @@ interface Props {
 export const StackIcon = ({ icon
 }: Props) => {
   return (
-    <div className="text-darkPink transition-transform duration-300 hover:scale-110">
+    <div className="text-darkPink transition-transform duration-300 hover:scale-120">
       <span className="text-xl md:text-4xl ">{icon}</span>
     </div>
   )
