d8aad5a287f694096455bef6b88163c42af2159c
Elynzx <rpascual06@outlook.com>
2026-03-11 15:27:41 -0500

    fix(HeroSection): improve layout and spacing for better readability

--- src/features/Home/sections/Hero/index.tsx
+++ src/features/Home/sections/Hero/index.tsx
@@ -2,6 +2,7 @@ import ProfilePhoto from "@/assets/home/ProfilePhoto/Profile_02.png";
 import HiIcon from "@/assets/home/hi.png";
 import { Badge } from './components/Badge'
 import { FaMedal, FaCertificate, FaUsers, FaReact, FaDocker } from "react-icons/fa";
+import { MdDownload } from "react-icons/md";
 import { SiTypescript, SiSpringboot, SiPython } from "react-icons/si";
 import { StackIcon } from './components/StackIcon'
 
@@ -44,10 +45,10 @@ const techStack = [
 export const Hero = () => {
   return (
     <section
-      className="grid grid-cols-1 gap-5 p-6 md:grid-cols-2 md:grid-rows-2 md:gap-2 md:px-10 items-center justify-center"
+      className="grid grid-cols-1 gap-6 py-6 px-8 md:grid-cols-2 md:grid-rows-2 md:gap-2 md:px-10 items-center justify-center"
     >
 
-      <div className="flex items-center text-xl gap-2 mt-3 justify-center md:mt-14 md:justify-start md:row-start-1 md:col-start-1">
+      <div className="flex items-center text-xl gap-2 mt-3 md:mt-0 justify-center md:justify-start md:row-start-1 md:col-start-1">
         <img
           src={about.images.hi}
           alt={"Greeting from " + about.name}
@@ -55,7 +56,7 @@ export const Hero = () => {
         />
         <div className="flex flex-col">
           <h2 className="text-lg font-semibold md:text-4xl md:mb-5 text-darkPink">
-          I'm {about.name}
+            I'm {about.name}
           </h2>
           <span className="text-sm font-semibold md:text-4xl">{about.role}</span>
         </div>
@@ -67,6 +68,7 @@ export const Hero = () => {
           alt={"Profile photo of " + about.name}
           className="w-32 h-38 md:w-70 md:h-85 z-10 md:mb-6 object-cover"
         />
+        
         <div className="flex flex-row gap-4 md:gap-7 mt-5 items-center justify-center">
           {techStack.map((tech, idx) => (
             <StackIcon key={idx} icon={tech.icon} />
@@ -74,8 +76,8 @@ export const Hero = () => {
         </div>
       </div>
 
-      <div className="flex flex-col text-justify items-center mt-4 gap-2 md:gap-5 md:mt-8 md:row-start-2 md:col-start-1">
-        <p className="italic text-xs border-l-3 pl-3 text-white md:text-xl">{about.description}</p>
+      <div className="flex flex-col text-justify items-center justify-center mt-4 gap-2 md:gap-8 md:mt-0 md:row-start-2 md:col-start-1">
+        <p className="italic text-xs w-full border-l-3 pl-3 text-white md:text-xl">{about.description}</p>
         <div className="flex flex-col gap-3 mt-4 md:flex-row md:gap-4">
           {badges.map((badge, idx) => (
             <Badge key={idx} icon={badge.icon} text={badge.text} />
