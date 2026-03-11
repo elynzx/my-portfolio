0c9901a6f7600ab3978184d7faca38382230e84e
Elynzx <rpascual06@outlook.com>
2026-03-11 11:16:20 -0500

    feat(hero): responsive grid layout for mobile and desktop

--- src/features/Home/sections/Hero/index.tsx
+++ src/features/Home/sections/Hero/index.tsx
@@ -1,4 +1,4 @@
-import ProfilePhoto from "@/assets/home/ProfilePhoto/Profile_01.png";
+import ProfilePhoto from "@/assets/home/ProfilePhoto/Profile_02.png";
 import HiIcon from "@/assets/home/hi.png";
 import { Badge } from './components/Badge'
 import { FaMedal, FaCertificate, FaUsers, FaReact, FaDocker } from "react-icons/fa";
@@ -26,10 +26,10 @@ const badges = [
     icon: <FaCertificate />,
     text: "English B2 certified"
   },
-  {
-    icon: <FaUsers />,
-    text: "IEEE WIE volunteer"
-  },
+  /*   {
+      icon: <FaUsers />,
+      text: "IEEE WIE volunteer"
+    }, */
 ]
 
 const techStack = [
@@ -43,35 +43,40 @@ const techStack = [
 
 export const Hero = () => {
   return (
-    <section className="flex flex-col items-center justify-center p-6">
-      <div className="flex items-center text-xl gap-2 md:flex-row md:items-end ">
+    <section
+      className="grid grid-cols-1 gap-5 p-6 md:grid-cols-2 md:grid-rows-2 md:gap-2 md:px-10 items-center justify-center"
+    >
+
+      <div className="flex items-center text-xl gap-2 mt-3 justify-center md:mt-14 md:justify-start md:row-start-1 md:col-start-1">
         <img
           src={about.images.hi}
           alt={"Greeting from " + about.name}
-          className="w-15 md:w-32 jello-vertical"
+          className="w-15 md:w-38 jello-vertical"
         />
         <div className="flex flex-col">
-          <h2 className="text-lg font-semibold md:text-4xl md:mb-5 md:ml-2 text-darkPink">
-            I'm {about.name}
+          <h2 className="text-lg font-semibold md:text-4xl md:mb-5 text-darkPink">
+          I'm {about.name}
           </h2>
-          <span className="text-sm font-semibold md:text-4xl ">{about.role}</span>
+          <span className="text-sm font-semibold md:text-4xl">{about.role}</span>
         </div>
       </div>
-      <img
-        src={about.images.profile}
-        alt={"Profile photo of " + about.name}
-        className="h-auto w-32 rounded-full md:w-90 z-10 md:mb-6"
-      />
 
-      <div className="flex flex-row gap-4 mt-6 items-center justify-center">
-        {techStack.map((tech, idx) => (
-          <StackIcon key={idx} icon={tech.icon} />
-        ))}
+      <div className="flex flex-col items-center justify-center md:row-span-2 md:col-start-2">
+        <img
+          src={about.images.profile}
+          alt={"Profile photo of " + about.name}
+          className="w-32 h-38 md:w-70 md:h-85 z-10 md:mb-6 object-cover"
+        />
+        <div className="flex flex-row gap-4 md:gap-7 mt-5 items-center justify-center">
+          {techStack.map((tech, idx) => (
+            <StackIcon key={idx} icon={tech.icon} />
+          ))}
+        </div>
       </div>
 
-      <div className="flex text-justify items-center flex-col mt-6 gap-2 md:mt-0 md:flex-row md:mb-16">
-        <p className="italic text-xs border-l-4 pl-2 text-white md:text-[17px] md:mr-28">{about.description}</p>
-        <div className="flex flex-col gap-3 mt-4">
+      <div className="flex flex-col text-justify items-center mt-4 gap-2 md:gap-5 md:mt-8 md:row-start-2 md:col-start-1">
+        <p className="italic text-xs border-l-3 pl-3 text-white md:text-xl">{about.description}</p>
+        <div className="flex flex-col gap-3 mt-4 md:flex-row md:gap-4">
           {badges.map((badge, idx) => (
             <Badge key={idx} icon={badge.icon} text={badge.text} />
           ))}
