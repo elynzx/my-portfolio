e01be7a72e6409a3d65f912e8c0d51b0f5dc524b
Elynzx <rpascual06@outlook.com>
2026-04-08 20:20:34 -0500

    feat: add new sidebar icons and integrate Sidebar component into Home

--- src/features/Home/index.tsx
+++ src/features/Home/index.tsx
@@ -2,6 +2,8 @@ import { useState, type ReactNode } from 'react'
 import { Hero, About, Education, Skills } from './sections'
 import { Pagination } from '@/shared/components'
 import { ClipDeco } from '@/shared/ui'
+import {Sidebar} from '@/shared/components'
+
 
 const HOME_SECTIONS: Record<string, ReactNode> = {
   hero: <Hero />,
@@ -18,14 +20,13 @@ export const Home = () => {
   return (
 
     <div className="flex h-full w-full md:relative">
-      {/*       <aside className="mt-20 ml-3">
-        {/* <Sidebar selected={activeSection} onSelect={setActiveSection} /> 
-      </aside> */}
+      <aside className="mt-20 ml-3">
+        <Sidebar selected={activeSection} onSelect={setActiveSection}  />
+      </aside>
       <section className="p-6 md:p-12 md:flex-1 w-full ">
         <div
           key={activeSection}
           className="h-full w-full rounded-xl bg-bgDeepBlack/60 slide-left md:ml-7 md:px-16 md:py-10 ">
-
           <ClipDeco />
           <div className="overflow-y-auto no-scrollbar">
             {section}
